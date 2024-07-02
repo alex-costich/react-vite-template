6bb90890d0f6fe1ca9cc744975ba3a5b574ca3ea
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-02 09:07:38 -0700

    Added images to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom'
 import './index.css'
 
-const app = <img src='logo.svg' />
+const app = <img src='react.svg' />
+console.log(app)
 const container = document.getElementById('root')
 
 createRoot(container).render(app)
